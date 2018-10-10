113a77b8d10329dcd09591b061b4b0cecd7488e7
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-10 03:31:30 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,13 +2,12 @@
 #require_relative './dance_module.rb'
 require_relative './fancy_dance.rb'
 class Dancer
-  include Dance
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
  
-  attr_accessor :name
+  #attr_accessor :name
  
-  def initialize(name)
-    @name = name
-  end
-  
-  extend MetaDancing
+  #def initialize(name)
+  #  @name = name
+  #end
 end
