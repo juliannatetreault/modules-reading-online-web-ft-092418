f741310d55cbcae3777b7dd2148edd9104c00683
Julianna Tetreault <juliannatetreault@icloud.com>
2018-10-10 03:30:08 +0000

    automatically backed up by learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,13 +1,12 @@
 require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 class Kid
-  include Dance
- 
-  attr_accessor :name
- 
-  def initialize(name)
-    @name = name
-  end
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
   
-  extend MetaDancing 
+  #attr_accessor :name
+ 
+  #def initialize(name)
+  #  @name = name
+  #end
 end
